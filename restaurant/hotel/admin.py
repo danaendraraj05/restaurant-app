0d5c310df1c17fa585e0c48f3164114c784b726d
danaendraraj05 <danaendraraj@testpress.in>
2024-07-02 11:18:46 +0530

    Created Models

--- restaurant/hotel/admin.py
+++ restaurant/hotel/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Cuisine, Restaurant, Photo, Dish
 
-# Register your models here.
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('title', 'rating', 'cost_for_two', 'owner', 'location', 'food_type')
+    list_filter = ('food_type', 'cuisines')
+    search_fields = ('title', 'location', 'owner')
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'image')
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'dish_type', 'restaurant')
+    list_filter = ('dish_type', 'restaurant')
+    search_fields = ('name',)
+
--- restaurant/hotel/models.py
+++ restaurant/hotel/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    VEG = 'Veg'
+    NON_VEG = 'Non-Veg'
+    VEGAN = 'Vegan'
+    FOOD_TYPE_CHOICES = [
+        (VEG, 'Vegetarian'),
+        (NON_VEG, 'Non-Vegetarian'),
+        (VEGAN, 'Vegan'),
+    ]
+
+    title = models.CharField(max_length=255)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    reviews = models.TextField()
+    cost_for_two = models.DecimalField(max_digits=6, decimal_places=2)
+    owner = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    address = models.TextField()
+    timings = models.CharField(max_length=100)
+    food_type = models.CharField(max_length=7, choices=FOOD_TYPE_CHOICES)
+    cuisines = models.ManyToManyField(Cuisine, related_name='restaurants')
+
+    def __str__(self):
+        return self.title
+
+class Photo(models.Model):
+    restaurant = models.ForeignKey(Restaurant, related_name='photos', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='restaurant_photos/')
+
+    def __str__(self):
+        return f"Photo for {self.restaurant.title}"
+
+class Dish(models.Model):
+    VEG = 'Veg'
+    NON_VEG = 'Non-Veg'
+    DISH_TYPE_CHOICES = [
+        (VEG, 'Vegetarian'),
+        (NON_VEG, 'Non-Vegetarian'),
+    ]
+
+    restaurant = models.ForeignKey(Restaurant, related_name='dishes', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.CharField(max_length=7, choices=DISH_TYPE_CHOICES)
+
+    def __str__(self):
+        return f"{self.name} ({self.dish_type}) - {self.restaurant.title}"
